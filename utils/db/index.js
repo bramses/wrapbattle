e4753acb9f7a3a8db18aa19c971c4bb61c1ed775
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-12-09 02:49:10 -0500

    index

--- utils/db/index.js
+++ utils/db/index.js
@@ -1,10 +1,12 @@
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
+// import serviceAccount from './serviceAccountKey.json';
 
 if (!admin.apps.length) {
   try {
+    const serviceAcctMinusPrivateKey = JSON.parse(process.env.FIREBASE_S_A_KEY);
+    serviceAcctMinusPrivateKey.private_key = process.env.FIREBASE_P_K;
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAcctMinusPrivateKey),
     });
   } catch (error) {
     console.log('Firebase admin initialization error', error.stack);
